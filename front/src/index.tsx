2375c58b7eb323b96eafebe4edb7100cc76ba6f8
c10t <c10t@fa.ke>
2018-10-16 09:29:17 +0900

    redux without typing

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,14 +2,28 @@ import App from 'components/App';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import configureStore from 'store';
 import setGlobalStyles from 'styles/globals';
 import registerServiceWorker from './registerServiceWorker';
 
 setGlobalStyles();
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+const renderApp = () => {
+  const initialState = {};
+  const store = configureStore(initialState);
+
+  store.dispatch({ type: '@myReader/@@INIT' });
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
+  );
+};
+
+renderApp();
+
 registerServiceWorker();
