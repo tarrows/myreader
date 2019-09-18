73f026be79cbe12e1e7fe61447c563d961bfa696
c10t <c10t@fa.ke>
2019-09-18 23:59:19 +0900

    setup test

--- rest/news/tests.py
+++ rest/news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from .models import Story
+
 # Create your tests here.
+
+class NewsTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_story_create_single(self):
+        pass
